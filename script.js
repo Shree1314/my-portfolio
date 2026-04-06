1c6a95414411a7daba729984a3a938da3ec0d152
Shree1314 <shreyas3637pl@gmail.com>
2026-04-06 17:39:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Dark Mode
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 }
+
+// Scroll Animation
+const faders = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+  faders.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
