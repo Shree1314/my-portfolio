81b1f0d89f4bbda5179f4b07227ec48b1801870a
Shree1314 <shreyas3637pl@gmail.com>
2026-04-06 22:10:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,16 @@ function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 }
 
-// Scroll animation
-const faders = document.querySelectorAll(".fade");
+function openImage(img) {
+  document.getElementById("popup").style.display = "flex";
+  document.getElementById("popup-img").src = img.src;
+}
 
-window.addEventListener("scroll", () => {
-  faders.forEach(el => {
-    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
-      el.classList.add("show");
-    }
-  });
-});
+function closeImage() {
+  document.getElementById("popup").style.display = "none";
+}
 
-// Typing effect
+// typing
 const text = "Aspiring Engineer | AI Enthusiast | Python Learner";
 let i = 0;
 
@@ -25,13 +23,3 @@ function type() {
   }
 }
 type();
-
-// Image popup
-function openImage(img) {
-  document.getElementById("popup").style.display = "flex";
-  document.getElementById("popup-img").src = img.src;
-}
-
-function closeImage() {
-  document.getElementById("popup").style.display = "none";
-}
