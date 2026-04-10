7bd25db5dfcbea9bf6d116eeeeb157247995a018
Shree1314 <shreyas3637pl@gmail.com>
2026-04-10 18:05:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const elements = document.querySelectorAll(".reveal");
 
 function show() {
   elements.forEach(el => {
-    if (el.getBoundingClientRect().top < window.innerHeight - 50) {
+    if (el.getBoundingClientRect().top < window.innerHeight - 80) {
       el.classList.add("active");
     }
   });
