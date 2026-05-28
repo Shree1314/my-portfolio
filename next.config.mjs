89fb1629711586ae05c34bc1e03c6008edb4dc77
Shree1403 <shreyas3637pl@gmail.com>
2026-05-28 16:15:18 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/my-portfolio',
+  assetPrefix: '/my-portfolio/',
 }
 
 export default nextConfig
